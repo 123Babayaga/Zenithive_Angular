c88c86fc6d94e7dfb18204d19164e2d82392a6ec
aayushp <aayushp@zenithive.com>
2025-04-15 15:19:00 +0530

    changes for added env

--- github-user-search/src/app/services/github.service.ts
+++ github-user-search/src/app/services/github.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { GitHubUser, GitHubSearchResponse } from '../models/github-user';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
